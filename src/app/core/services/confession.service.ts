31ce34c9ee355ff23ef55cbeeec9c72d5771b0fb
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-07-09 01:35:43 +0000

    added: api call to fetch confessions and render them

--- src/app/core/services/confession.service.ts
+++ src/app/core/services/confession.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Confession } from "../../models/confession.model";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfessionService {
+  private baseUrl = `${environment.apiUrl}/confessions`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getConfessions(): Observable<{ status: string; data: Confession[] }> {
+    return this.http.get<{ status: string; data: Confession[] }>(this.baseUrl);
+  }
+  updateConfession(id: number, data: Partial<Confession>): Observable<any> {
+  return this.http.put(`${this.baseUrl}/${id}`, data);
+}
+
+
+  deleteConfession(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
 }
--- src/app/models/confession.model.ts
+++ src/app/models/confession.model.ts
@@ -1,2 +1,9 @@
 export interface Confession {
+  id: number;
+  message: string;
+  category: string;
+  emotion: string;
+  is_approved: boolean;
+  created_at: string;
+  updated_at: string;
 }
