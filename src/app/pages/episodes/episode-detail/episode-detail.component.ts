9691154d52356d333baa3a21e657bdc69ef03dfb
cobbygraves <rmeocnis@hotmail.com>
2025-07-09 09:40:08 +0000

    feat:add detail page ui template

--- src/app/pages/episodes/episode-detail/episode-detail.component.ts
+++ src/app/pages/episodes/episode-detail/episode-detail.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { MobileAdminMenuComponent } from '../../../shared/components/mobile-admin-menu/mobile-admin-menu.component';
-import {
-  Home,
-  MessageSquare,
-  List,
-  Play
-} from 'lucide-angular';
+import { Home, MessageSquare, List, Play } from 'lucide-angular';
+import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-episode-detail',
-  imports: [MobileAdminMenuComponent],
+  imports: [MobileAdminMenuComponent, ButtonModule, HeaderComponent],
   templateUrl: './episode-detail.component.html',
-  styleUrl: './episode-detail.component.scss'
+  styleUrl: './episode-detail.component.scss',
 })
 export class EpisodeDetailComponent {
   menuItems = [
--- src/app/shared/components/playlist-card/playlist-card.component.ts
+++ src/app/shared/components/playlist-card/playlist-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Episode } from '../../../models/episode.model';
 import { DatePipe } from '@angular/common';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-playlist-card',
@@ -11,4 +12,9 @@ import { CommonModule } from '@angular/common';
 })
 export class PlaylistCardComponent {
   episode = input<Episode>();
+  router = inject(Router);
+
+  onDetails(id: number | undefined) {
+    this.router.navigate(['/episodes', id]);
+  }
 }
